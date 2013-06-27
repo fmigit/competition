bac426ea22fc48c668f74b7687d30dbb761212f8
fmallouli <fmallouli@acs-ami.com>
2013-06-27 11:24:00 +0200

    ajout dateCreation dans club (2eme commit)

--- src/com/acs/model/Club.java
+++ src/com/acs/model/Club.java
@@ -5,6 +5,9 @@ public class Club {
 	//1er commit dans branche GestionClub
 	private String nom;
 	
+	//2eme commit dans branche GestionClub
+	private String dateCreation;
+	
 	public void Club(){
 		
 	}
